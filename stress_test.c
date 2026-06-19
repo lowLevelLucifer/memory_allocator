e19f89c764e56f51252055ce03b4035ab42452ce
lowLevelLucifer <uthkarsh75@gmail.com>
2026-06-19 16:36:19 +0000

    added rounds for the stress test

--- stress_test.c
+++ stress_test.c
@@ -4,13 +4,14 @@
 int main(){
 	void *ptrs[10000];
 	int num = 0;
-	for(int i = 0 ; i < 10000; i++){
-		num = rand() % 1017 + 8;
-		malloc(num);
-		*ptrs[i] = num;
-	}
-	for(int i = 0 ; i < 10000; i++){
-		free(*ptrs[i]);
+	for(int round = 0; round < 5; round++){
+		for(int i = 0 ; i < 10000; i++){
+			num = rand() % 1017 + 8;
+			ptrs[i] = malloc(num);
+		}
+		for(int i = 0 ; i < 10000; i++){
+			free(ptrs[i]);
+		}
 	}
 	return 0;
 }
